78493fc93128fcc4cb180e88abd12313bfe97715
daniel-monaghan-purple-beard <daniel_m@purplebeard.co.uk>
2023-04-25 18:33:50 +0100

    added IIFE Example

---
+++ iifeExample.js
@@ -0,0 +1,31 @@
+// IIFE Example
+
+// An IIFE is a function that is 
+// defined and called on the same line.
+
+// 
+((a) => { console.log('input iife:', a) })("pop");
+
+const varName = "Hello";
+
+(function () {
+  console.log("I Run straight away")
+  return "secret"
+})();
+
+console.log(varName);
+
+(() => {
+  console.log("arrow function")
+})();
+
+function nameOfFunction() {
+  return true
+}
+
+nameOfFunction()
+
+const nameOfArrowFunction = () => {
+  return true
+}
+nameOfArrowFunction()
